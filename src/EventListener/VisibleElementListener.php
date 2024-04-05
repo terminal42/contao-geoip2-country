d2c4b6b31cac66376ba8f1448671947e2d563b1c
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-04-05 11:23:37 +0200

    Always show elements in the back end

--- src/EventListener/VisibleElementListener.php
+++ src/EventListener/VisibleElementListener.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Terminal42\Geoip2CountryBundle\EventListener;
 
+use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\Model;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Terminal42\Geoip2CountryBundle\CountryProvider;
 
@@ -13,11 +15,16 @@ class VisibleElementListener
     public function __construct(
         private readonly CountryProvider $countryProvider,
         private readonly RequestStack $requestStack,
+        private readonly ScopeMatcher $scopeMatcher,
     ) {
     }
 
     public function __invoke(Model $element, bool $hasAccess): bool
     {
+        if ($this->scopeMatcher->isBackendRequest($this->requestStack->getCurrentRequest() ?? Request::create(''))) {
+            return $hasAccess;
+        }
+
         if ('show' !== $element->geoip_visibility && 'hide' !== $element->geoip_visibility) {
             return $hasAccess;
         }
